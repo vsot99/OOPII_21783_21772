5b72723992b9bd9f5d8d7267c56711a9d047e0e3
VassilisSot99 <80210010+VassilisSot99@users.noreply.github.com>
2021-04-15 21:18:09 +0300

    Add files via upload

---
+++ JavaHuaProject/src/YoungTraveller.java
@@ -0,0 +1,25 @@
+import java.util.Collections;
+
+public class YoungTraveller extends Traveller {
+	
+    public YoungTraveller(int age, String city) {
+        super(age, city);
+        this.setP(0.95);
+        for ( City c : this.getCities() ) {
+        	c.setSimilarity(City.calculate_similarity(this, c)); //sets the similarity for each city
+        }
+        Collections.sort(this.getCities(), Collections.reverseOrder());
+    }
+    
+    
+    public double similarity_terms_vector(City city) {
+           
+           int sum=0;
+
+           for(int i=0;i < this.getPreferencesVector().length; i++){
+             sum+= Math.pow(this.getPreferencesVector()[i]-city.getTermsVector()[i], 2);
+           }
+           return 1/(1+Math.sqrt(sum));
+    }
+
+}
